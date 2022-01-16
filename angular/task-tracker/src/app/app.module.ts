e2208189c238a8287cc3f54d0ba65979a3212481
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-01-16 14:27:54 +0600

    hello angular modified from home and a new task-tracker project created

--- angular/task-tracker/src/app/app.module.ts
+++ angular/task-tracker/src/app/app.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent,
-    DataBindingComponent,
-    TodoListComponent,
     HeaderComponent,
     ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

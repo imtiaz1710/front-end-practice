82731bf0d7a9bd9b17d5549f50cdc665eea4cb20
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-02-03 15:08:53 +0600

    working ngx datable for users but based on wrong business logic

--- angular/fine-management/src/app/user/users/users.component.ts
+++ angular/fine-management/src/app/user/users/users.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { UserService } from 'src/app/services/user.service';
-import { User } from '../user';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { MyProfileService } from 'src/app/services/my-profile.service';
 import { UserTeamService } from 'src/app/services/user-team.service';
@@ -55,22 +54,17 @@ export class UsersComponent implements OnInit {
   }
 
   onSelect() {
-    // this.rows = [];
-    this.userTeamService
-      .getUserTeamByTeamId(<number>this.selectTeamForm.value.teamId)
-      .subscribe({
-        next: (userTeam: UserTeam[]) => {
-          userTeam.forEach((element) => {
-            debugger;
-            this.userService.getUserById(element.userId).subscribe({
-              next: (user: User) => {
-                debugger;
-                this.rows.push(user);
-              },
-              error: (err) => console.log(err),
-            });
-          });
-          debugger;
+    let promise1, promises = [];
+    promise1 = this.getUserTeamByTeamId(this.selectTeamForm.value.teamId);
+
+    promise1.then((res) => {
+      res.forEach((element) => {
+        promises.push(this.getUserById(element.userId));
+      });
+
+      Promise.all(promises)
+        .then((user) => {
+          this.rows = user;
           this.rows = this.rows.map(
             (x) =>
               (x = {
@@ -80,10 +74,105 @@ export class UsersComponent implements OnInit {
                 id: x.id,
               })
           );
-          
+        })
+    });
+
+    // promise1.then((userTeams: UserTeam[]) => {
+    //   debugger
+    //   userTeams.map((userTeam) => {
+    //     promises.push(this.getUserById(userTeam.userId));
+    //     // Promise.all([promise1, promise2]).then(() => {
+    //     //   promise2.then((row) => {
+    //     //     let data = {};
+    //     //     data['id'] = row.id;
+    //     //     data['name'] = row.name;
+    //     //     data['email'] = row.email;
+    //     //     data['phoneNo'] = row.phoneNo;
+    //     //     this.rows.push(data);
+    //     //   });
+    //     // });
+    //   });
+    // });
+
+    // Promise.all([promise1,...promises]).then((res) => {
+
+    //   res.map((p) => {
+    //     this.rows.push(p);
+    //   });
+    //   debugger
+    //   this.rows = this.rows.map(
+    //     (x) =>
+    //       (x = {
+    //         name: x.name,
+    //         email: x.email,
+    //         phoneNo: x.phoneNo,
+    //         id: x.id,
+    //       })
+    //   );
+    // });
+
+    // let userPromises = [];
+    // userPromises.push(
+    //   this.getUserTeamByTeamId(this.selectTeamForm.value.teamId).then(
+    //     (userTeams: UserTeam[]) => {
+    //       userTeams.forEach((element) => {
+    //         userPromises.push(this.getUserById(element.userId));
+    //       });
+    //     }
+    //   )
+    // );
+
+    // Promise.all(userPromises).then((result) => {
+    //   debugger;
+    //   //this.rows.push();
+    // });
+    // this.rows = [];
+    //   this.userTeamService
+    //     .getUserTeamByTeamId(<number>this.selectTeamForm.value.teamId)
+    //     .subscribe({
+    //       next: (userTeam: UserTeam[]) => {
+    //         userTeam.forEach((element) => {
+    //           this.userService.getUserById(element.userId).subscribe({
+    //             next: (user: User) => {
+    //               this.rows.push(user);
+    //             },
+    //             error: (err) => console.log(err),
+    //           });
+    //         });
+    //       },
+    //       error: (err) => console.log(err),
+    //     });
+    // this.rows = this.rows.map(
+    //   (x) =>
+    //     (x = {
+    //       name: x.name,
+    //       email: x.email,
+    //       phoneNo: x.phoneNo,
+    //       id: x.id,
+    //     })
+    // );
+  }
+
+  getUserById(userId: number) {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserById(userId).subscribe(
+        (result) => {
+          resolve(result);
         },
-        error: (err) => console.log(err),
-      });
+        (err) => reject(err)
+      );
+    });
+  }
+
+  getUserTeamByTeamId(teamId: number) {
+    return new Promise((resolve, reject) => {
+      this.userTeamService.getUserTeamByTeamId(teamId).subscribe(
+        (result) => {
+          resolve(result);
+        },
+        (err) => reject(err)
+      );
+    });
   }
 
   delete(value) {}

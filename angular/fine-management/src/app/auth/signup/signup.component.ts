6cbf6e55baa9dbe94e12b051fc352311a9032e2b
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-02-14 21:11:34 +0600

    email validation add of signup form

--- angular/fine-management/src/app/auth/signup/signup.component.ts
+++ angular/fine-management/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { Team } from '../../models/team';
@@ -12,33 +12,43 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-  public signupForm!: FormGroup;
+  signupForm!: FormGroup;
+  submitted: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
     private router: Router,
     private toasterService: ToastrService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
-      email: [''],
-      password: [''],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(40)]],
+      password: ['',],
       confirmPassword: ['']
     });
   }
 
+  get signupFormControls(): { [key: string]: AbstractControl } {
+    return this.signupForm.controls;
+  }
+
   signUp() {
-    this.http
-      .post<any>('http://localhost:3000/users', this.signupForm.value)
-      .subscribe({
-        next: (res: any) => {
-          this.toasterService.success('Sign up Successful!');
-          this.signupForm.reset();
-          this.router.navigate(['login']);
-        },
-        error: (err: any) => this.toasterService.error('Error!'),
-      });
+    this.submitted = true;
+
+    if (this.signupForm.valid) {
+      this.submitted = false;
+      this.http
+        .post<any>('http://localhost:3000/users', this.signupForm.value)
+        .subscribe({
+          next: (res: any) => {
+            this.toasterService.success('Sign up Successful!');
+            this.signupForm.reset();
+            this.router.navigate(['login']);
+          },
+          error: (err: any) => this.toasterService.error('Error!'),
+        });
+    }
   }
 }
